be378c36c4b547646716f6fe761e056fdf8cfbd0
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-11-08 22:56:52 +0100

    Change style.css

--- scripts.js
+++ scripts.js
@@ -19,10 +19,6 @@ function createTweet(input) {
 
    	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
-   	if (tweetText.length > 140) {
-    	getQuote();
-	}
-
 	if (tweetText.length > 140) {
     	getQuote();
 	} else {
